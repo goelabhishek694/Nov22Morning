ca08b1f3f2f50722d9d0272bc196e57079f1f79a
Abhishek <abhishekgoel@gofynd.com>
2024-12-16 09:30:54 +0530

    rest

--- JS/Intro/6. spread_rest_default.js
+++ JS/Intro/6. spread_rest_default.js
@@ -1,11 +1,11 @@
 //default paramater
-function fn(param1, param2,param3="sayo nara"){
-    console.log("hi params are", param1,param2,param3);
+// function fn(param1, param2,param3="sayo nara"){
+//     console.log("hi params are", param1,param2,param3);
     
-}
+// }
 
-fn("hi","hello","bye");
-fn("hi","hello");
+// fn("hi","hello","bye");
+// fn("hi","hello");
 
 //spread operator 
 // let arr = [1,2,3,4,5];
@@ -50,4 +50,30 @@ fn("hi","hello");
 // let newObj = {...obj}
 // spread operator shallow copies and not deep copies. 
 
+
+//rest parameter
+// it is used to collect n number of params . syntax of rest and spread is same ... 
+//they are used in different places. rest is used while accepting parameters in a function 
+
+function fn(param1, ...param2){
+    console.log(param1);
+    console.log(param2);
+}
+
+// fn("hi","hello");
+fn("hi","hello", "bye", "sayo nara", "ni hao");
+
+function sumAll(...numbers){
+    console.log(numbers.length);
+    let sum =0;
+    for(let i=0;i<numbers.length;i++){
+        sum = sum +numbers[i]
+    }
+
+    return sum;
+    
+}
+
+console.log(sumAll(10,20));
+console.log(sumAll(10,20,30,40));
 
