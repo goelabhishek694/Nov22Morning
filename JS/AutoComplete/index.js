5da4f4deccd5cd595ea0e4bf5bb94c353e81b809
Abhishek <abhishekgoel@gofynd.com>
2025-02-05 07:59:29 +0530

    autocomplete completed

--- JS/AutoComplete/index.js
+++ JS/AutoComplete/index.js
@@ -18,6 +18,47 @@ const handleSuggestion = async (e) => {
     
 }
 
-inputBox.addEventListener("input", handleSuggestion);
+// inputBox.addEventListener("input", handleSuggestion);
 
+function populateSuggestionBox(countryNameArr){
+    if(countryNameArr.length > 0){
+        //suggestionBox to be visible 
+        suggestionBox.classList.add("visible");
+    }else {
+        //hide the suggestion box
+        suggestionBox.classList.remove("visible");
+    }
+
+    //to remove the prev results
+    suggestionBox.innerHTML="";
+
+    const fragment = document.createDocumentFragment();
+
+    countryNameArr.forEach((country) => {
+        const li = document.createElement("li");
+        li.innerText = country;
+        fragment.appendChild(li);
+    })
+
+    suggestionBox.appendChild(fragment);
+
+}
+
+
+function debounce(fn, delay = 300){
+    let timerId;
+    return function(...args){
+        if(timerId){
+            //clear the prev timerId
+            console.log("i am reseting");
+            clearTimeout(timerId);
+        }
+        timerId = setTimeout(() => {
+            //api call
+            fn(...args)
+        }, delay)
+    }
+}
+
+inputBox.addEventListener("input", debounce(handleSuggestion));
 
