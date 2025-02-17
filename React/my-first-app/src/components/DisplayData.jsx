adb5cf843fe794d4842ad3fb19c31b33c0ab8d93
Abhishek <abhishekgoel@gofynd.com>
2025-02-17 09:10:10 +0530

    done

--- React/my-first-app/src/components/DisplayData.jsx
+++ React/my-first-app/src/components/DisplayData.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React from "react";
 
-function DisplayData({fruits, buyer}) {
-    console.log(fruits, buyer);
-    
-//   code below is JSX
-    return (
+function DisplayData({ fruits, buyer }) {
+  console.log(fruits, buyer);
+
+  //   code below is JSX
+  return (
     <div>
       <h2>Fruits List</h2>
       <ul>
-        {
-            fruits.map((fruit, idx) => <li key={idx} >{fruit}</li>)
-        }
+        {/* single line  */}
+      {/* {fruits.map((fruit, idx) => <li key={idx}>{fruit}</li>)} */}
+{/* some processing  */}
+        {fruits.map((fruit, idx) => {
+          fruit = fruit.toUpperCase();
+          return <li key={idx}>{fruit}</li>;
+        })}
+        {/* return an array of html */}
+        {/* {
+          [<li>{fruits[0]}</li>, <li>{fruits[1]}</li>, <li>{fruits[2]}</li>]
+        } */}
       </ul>
 
       <h2>Buyer Details</h2>
-      {
-        Object.keys(buyer).map(key => <p>{key}: {buyer[key]}</p>)
-      }
+      {Object.keys(buyer).map((key) => (
+        <p>
+          {key}: {buyer[key]}
+        </p>
+      ))}
     </div>
-  )
+  );
 }
 
-export default DisplayData
+export default DisplayData;
