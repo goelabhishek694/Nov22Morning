67e0c67eb0f063eeb25224ef6a9c55e99f58023d
Abhishek <abhishekgoel@gofynd.com>
2025-03-03 08:40:21 +0530

    sorting searching filtering

--- React/imdbClone/src/components/WatchList.jsx
+++ React/imdbClone/src/components/WatchList.jsx
@@ -1,55 +1,146 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import genre from "../utility";
 function WatchList() {
-  const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [] ;
+  const [watchlist, setWatchlist] = useState([]);
+  const [search, setSearch] = useState("");
+  const [genreList, setGenreList] =  useState([]);
+  const [currGenre, setCurrGenre] =  useState("All Genres");
+
+  useEffect(() => {
+    let data = JSON.parse(localStorage.getItem("watchlist")) || [];
+    setWatchlist(data);
+  }, []);
+  const handleAscendingRating = () => {
+    console.log("sort low to high");
+    watchlist.sort((movie1, movie2) => {
+      return movie1.vote_average - movie2.vote_average;
+    });
+    console.log(watchlist);
+
+    setWatchlist([...watchlist]);
+  };
+
+  const handleDescendingRating = () => {
+    console.log("sort high to low");
+    watchlist.sort((movie1, movie2) => {
+      return movie2.vote_average - movie1.vote_average;
+    });
+    console.log(watchlist);
+
+    setWatchlist([...watchlist]);
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    let temp = watchlist.map(movie=>genre[movie.genre_ids[0]]);
+    console.log(temp);
+    temp = new Set(temp);
+    console.log(temp);
+    setGenreList(["All Genres", ...temp]);
+    
+  },[watchlist]);
+
+  const handleFilter = (genre) => {
+    console.log(genre);
+    
+    setCurrGenre(genre);
+  }
   return (
-    <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+    <>
+      <div className="flex justify-center mx-4 gap-8 my-4">
+        {
+      genreList.map((genre, idx) =>  (
+        <div 
+        key = {idx}
+        className= { currGenre == genre ? "flex justify-center items-center bg-blue-400 h-[3rem] w-[9rem] text-white font-bold rounded-xl" : "flex justify-center items-center bg-gray-400/50 h-[3rem] w-[9rem] text-white font-bold rounded-xl"}
+        onClick={() => handleFilter(genre)}
+        >{genre}</div>
+        ))
+      }
+      </div>
+    
+      {/* search movies */}
+      <div>
+        <input 
+        type="text"
+        className="h-[3rem] w-[18rem] bg-gray-200 px-4 outline-none border border-slate-600" 
+        placeholder="Search Movies"
+        onChange={handleSearch}
+        value={search}
+        />
+      </div>
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
         <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-            <thead>  
-                <tr className="bg-gray-50">
-                    <th className="px-6 py-4 font-medium text-gray-900">Name</th> 
-                    <th>
-                        <div className="flex">
-                            <div>Ratings</div>
-                        </div>
-                    </th>
-                    <th>
-                        <div className="flex">
-                            <div>Popularity</div>
-                        </div>
-                    </th>
-                    <th>
-                        <div className="flex">
-                            <div>Genre</div>
-                        </div>
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-100 border-t border-gray-100">  
-              {
-                watchlist.map(({title, popularity, vote_average, genre_ids, backdrop_path }, idx) => {
-                  return (<tr className="hover:bg-gray-50" key={idx}>
+          <thead>
+            <tr className="bg-gray-50">
+              <th className="px-6 py-4 font-medium text-gray-900">Name</th>
+              <th>
+                <div className="flex">
+                  <i
+                    class="fa-solid fa-arrow-up"
+                    onClick={handleAscendingRating}
+                  ></i>
+                  <div>Ratings</div>
+                  <i
+                    class="fa-solid fa-arrow-down"
+                    onClick={handleDescendingRating}
+                  ></i>
+                </div>
+              </th>
+              <th>
+                <div className="flex">
+                  <div>Popularity</div>
+                </div>
+              </th>
+              <th>
+                <div className="flex">
+                  <div>Genre</div>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {watchlist
+            .filter((movieObj) => {
+              if(currGenre == "All Genres"){
+                return true;
+              }else{
+                return genre[movieObj.genre_ids[0]] == currGenre
+              }
+            })
+            .filter(movieObj => movieObj.title.toLowerCase().includes(search.toLowerCase()))
+            .map(
+              (
+                { title, popularity, vote_average, genre_ids, backdrop_path },
+                idx
+              ) => {
+                return (
+                  <tr className="hover:bg-gray-50" key={idx}>
                     <td className="flex items-center px-6 py-4 font-normal text-gray-900">
-                        <img className="h-[6rem] w-[10rem] object-fit" src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt="" />
-                        <div className="font-medium text-gray-700 text-sm">{title}</div>
+                      <img
+                        className="h-[6rem] w-[10rem] object-fit"
+                        src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+                        alt=""
+                      />
+                      <div className="font-medium text-gray-700 text-sm">
+                        {title}
+                      </div>
                     </td>
-                    <td className="pl-6 py-4">
-                        {vote_average}
-                    </td>
-                    <td className="pl-6 py-4">
-                        {popularity}
-                    </td>
-                    <td className="pl-2 py-4">
-                        Action
-                    </td>
-                </tr>)
-                })
-                
-}
-            </tbody>
+                    <td className="pl-6 py-4">{vote_average}</td>
+                    <td className="pl-6 py-4">{popularity}</td>
+                    <td className="pl-2 py-4">{genre[genre_ids[0]]}</td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
         </table>
-    </div>
-);
+      </div>
+    </>
+  );
 }
 
-export default WatchList
+export default WatchList;
