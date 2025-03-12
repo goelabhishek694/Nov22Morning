19f3c033e7ae22fce05142c0d4aa4ff41d9cf7da
Abhishek <abhishekgoel@gofynd.com>
2025-03-12 07:48:52 +0530

    basic react app with bundle.js

---
+++ React/reactperf/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
