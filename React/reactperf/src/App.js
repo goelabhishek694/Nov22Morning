2f5ce791381c313d9d902f61721d351e9b75808a
Abhishek <abhishekgoel@gofynd.com>
2025-03-12 08:07:05 +0530

    code splitting using dyanmic imports

--- React/reactperf/src/App.js
+++ React/reactperf/src/App.js
@@ -1,18 +1,61 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
-import Home from "./component/Home";
-import About from "./component/About";
-import Contact from "./component/Contact";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import Navbar from "./component/Navbar";
+import { useEffect, useState } from "react";
+// import Home from "./component/Home";
+// import About from "./component/About";
+// import Contact from "./component/Contact";
 function App() {
+  const [Home, setHome] = useState(null);
+  const [About, setAbout] = useState(null);
+  const [Contact, setContact] = useState(null);
+
+  useEffect(() => {
+    //preload the home page component
+    //import function loads the comp async
+    import("./component/Home").then((module) =>
+      setHome(() => module.default)
+    );
+  }, []);
+
+  const loadAboutPage = () => {
+    import("./component/About").then((module) =>
+      setAbout(() => module.default)
+    );
+  }
+
+  const loadHomePage = () => {
+    import("./component/Home").then((module) =>
+      setHome(() => module.default)
+    );
+  }
+
+  const loadContactPage = () => {
+    import("./component/Contact").then((module) =>
+      setContact(() => module.default)
+    );
+  }
+
   return (
     <Router>
       <div>
-        <Navbar />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" onClick={loadHomePage}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={loadAboutPage}>About</Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={loadContactPage}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={Home ? <Home/> : <div>...Loading</div>} />
+          <Route path="/about" element={About ? <About/> :  <div>...Loading</div>} />
+          <Route path="/contact" element={Contact ? <Contact /> :  <div>...Loading</div>} />
         </Routes>
       </div>
     </Router>
