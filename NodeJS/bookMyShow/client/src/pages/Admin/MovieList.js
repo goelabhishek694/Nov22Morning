f73a2e5cfeee1bf64e2dd40f74a99af4c9929e4f
Abhishek <abhishekgoel@gofynd.com>
2025-04-21 08:55:50 +0530

    basic FE

--- NodeJS/bookMyShow/client/src/pages/Admin/MovieList.js
+++ NodeJS/bookMyShow/client/src/pages/Admin/MovieList.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import {Table} from 'antd'
+import React, { useState } from 'react'
+import {Button, Table} from 'antd'
+import { getAllMovies } from '../../calls/movies'
+import {showLoading, hideLoading} from "../../redux/loaderSlice";
+import {useDispatch} from "react-redux"
+import moment from "moment"
+import {EditOutlined, DeleteOutlined} from "@ant-design/icons"
 function MovieList() {
-    const movies = [
-        {
-          key: '1',
-          poster: 'Image1',
-          name: 'Mastaney',
-          description: 'Set in 1739, Nadar Shah`s undefeated army was attacked by Sikh Rebellions. ',
-          duration: 120,
-          genre: "Action",
-          language: "Hindi",
-          releaseDate: "Oct  25, 2023",
-        },
-        {
-          key: '2',
-          poster: 'Image2',
-          name: 'Mastaney',
-          description: 'Set in 1739, Nadar Shah`s undefeated army was attacked by Sikh Rebellions. ',
-          duration: 120,
-          genre: "Action",
-          language: "Hindi",
-          releaseDate: "Oct  25, 2023",
-          action: "Delete"
-        },
-
-      ];
+  const [movies, setMovies] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [formType, setFormType] = useState("add");
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const getData = async() => {
+    dispatch(showLoading());
+    const response = await getAllMovies();
+    const allMovies = response.data;
+    setMovies(allMovies.map(item => {
+      return {...item, key: `movie${item._id}`}
+    }));
+    dispatch(hideLoading());
+  }
 
       const tableHeadings = [
       {
-        title: "Poster"
+        title: "Poster",
+        dataIndex: "poster",
+        render: (text, data) => {
+          return (
+            <img 
+            width="75"
+            height="115"
+            style={{objectFit: 'cover'}}
+            src={data.poster}
+            alt="movie poster"
+            />
+          )
+        }
       },
       {
         title: "Movie Name",
@@ -36,11 +44,14 @@ function MovieList() {
       },
       {
         title: "Description",
-        dataIndex: "description"
+        dataIndex: "description",
       },
       {
         title: "Duration",
-        dataIndex: "duration"
+        dataIndex: "duration",
+        render: (text) => {
+          return `${text} Min`
+        }
       },
       {
         title: "Genre",
@@ -52,10 +63,31 @@ function MovieList() {
       },
       {
         title: "Release Date",
-        dataIndex: "releaseDate"
+        dataIndex: "releaseDate",
+        render: (text, data) => {
+          return moment(data.releaseDate).format("MM-DD-YYYY");
+        }
+
       },
       {
-        title: "Action"
+        title: "Action",
+        render: (text, data) => {
+          return (
+            <div>
+              <Button 
+              onClick={() => {
+                setIsModalOpen(true);
+                setSelectedMovie(data);
+                setFormType("edit");
+              }}><EditOutlined/></Button>
+              <Button 
+              onClick={() => {
+                setIsDeleteModalOpen(true);
+                setSelectedMovie(data);
+              }}><DeleteOutlined/></Button>
+            </div>
+          )
+        }
       }
       ]
   return (
